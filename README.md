e90d584a33d0302d2a385e643adc2b81e8b73587
j5-yuyu <2034369572@qq.com>
2025-06-18 00:17:27 +0800

    框架

--- README.md
+++
@@ -1,2 +0,0 @@
-# SubTrackPlat
-智能巡线车云端数据平台
---
+++ defectManager/src/main/java/com/defectmanager/DefectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.defectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefectManagerApplication.class, args);
+    }
+
+}
